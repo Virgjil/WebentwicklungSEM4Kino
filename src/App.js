1142b56922098ed40293030742d5f341570b4ce9
Virgjil <72380670+Virgjil@users.noreply.github.com>
2021-04-13 19:33:17 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import First from "./components/First";
+import React, { Component } from "react";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>jiz</h1> 
+        <First/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/First.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+class First extends Component {
+    state = {  }
+    render() {
+        return ( <h1>Hallo</h1>
+            
+        );
+    }
+}
+
+export default First;
